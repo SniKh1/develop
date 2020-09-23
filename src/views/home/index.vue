023a762620b0ac5c768a6bbf46f57ca6a7edd386
SniKh1 <1224074384@qq.com>
2020-09-23 18:32:27 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,10 +2,29 @@
 
 <template>
   <div class="index-container">
-    <van-tabs line-width="20" line-height="4" color="#ffc53d" background="#333" v-model="active" class="van_tabs" sticky swipeable>
-      <van-tab title-style="font:16px;color:#999" title="放映厅">放映厅111</van-tab>
-      <van-tab title-style="font:16px;color:#999" title="推荐">推荐</van-tab>
-      <van-tab title-style="font:16px;color:#999" title="专题">专题</van-tab>
+    <van-tabs line-width="20" line-height="4" color="#ffc53d" title-active-color="#000" background="#333" v-model="active" class="van_tabs"
+              @click="tab" sticky swipeable azy-render>
+      <van-tab>
+        <template #title>
+          <span :style="active===0?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
+            放映厅
+          </span>
+        </template>
+      </van-tab>
+      <van-tab>
+        <template #title>
+          <span :style="active===1?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
+            推荐
+          </span>
+        </template>
+      </van-tab>
+      <van-tab>
+        <template #title>
+          <span :style="active===2?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
+            专题
+          </span>
+        </template>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
@@ -13,13 +32,13 @@
 <script>
 import Vue from 'vue'
 import { Tab, Tabs } from 'vant'
-
+import { Toast } from 'vant';
 Vue.use(Tab)
 Vue.use(Tabs)
 export default {
   data() {
     return {
-      active: 0
+      active: 1
     }
   },
 
@@ -27,7 +46,10 @@ export default {
 
   mounted() { },
 
-  methods: {}
+  methods: {
+    tab(name, title) {
+    }
+  }
 }
 </script>
 
