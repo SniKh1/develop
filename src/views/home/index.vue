63365b09bbd2386d8aae30f8034b8ab7fd502666
SniKh1 <1224074384@qq.com>
2020-09-25 19:26:33 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,42 +3,59 @@
 <template>
   <div class="index-container">
     <van-tabs line-width="20" line-height="4" color="#ffc53d" title-active-color="#000" background="#333" v-model="active" class="van_tabs"
-              @click="tab" sticky swipeable azy-render>
+              @click="tab" @disabled="onClickDisabled" sticky swipeable azy-render>
+      <van-tab disabled>
+      </van-tab>
       <van-tab>
         <template #title>
-          <span :style="active===0?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
+          <span :style="active===1?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
             放映厅
           </span>
         </template>
       </van-tab>
       <van-tab>
         <template #title>
-          <span :style="active===1?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
+          <span :style="active===2?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
             推荐
           </span>
+        </template>
+        <template>
+          <div class="screen_box">
+            <screen></screen>
+          </div>
         </template>
       </van-tab>
       <van-tab>
         <template #title>
-          <span :style="active===2?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
+          <span :style="active===3?'color:#ffc53d;font-size:18px':'color:#999;font-size:16px'">
             专题
           </span>
         </template>
       </van-tab>
+      <van-tab disabled>
+        <template #title>
+          <div class="search_icon_box">
+            <img src="../../../static/search_icon.png" alt="">
+          </div>
+        </template>
+      </van-tab>
+
     </van-tabs>
+
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import { Tab, Tabs } from 'vant'
-import { Toast } from 'vant';
+import { Toast } from 'vant'
+import screen from './screen'
 Vue.use(Tab)
 Vue.use(Tabs)
 export default {
   data() {
     return {
-      active: 1
+      active: 2
     }
   },
 
@@ -48,7 +65,13 @@ export default {
 
   methods: {
     tab(name, title) {
-    }
+    },
+    onClickDisabled(name, title) {
+      Toast(name + '已被禁用');
+    },
+  },
+  components: {
+    'screen': screen  //将别名demo 变成 组件 Demo
   }
 }
 </script>
---
+++ src/views/home/screen.vue
@@ -0,0 +1,63 @@
+<!-- home -->
+
+<template>
+  <div class="screen-container">
+    <van-swipe :autoplay="3000" @change="handleChange">
+      <van-swipe-item v-for="(image, index) in images" :key="index" class="swipe_box">
+        <img class="swipe_item" v-lazy="image" />
+      </van-swipe-item>
+      <template #indicator>
+        <div class="custom-indicator">
+          <div class="van-swipe__indicators">
+            <i class="van-swipe__indicator" v-for="(item,index) in images" :class="{ 'van-swipe__indicator--active': index === mySwipeIndex }"></i>
+          </div>
+        </div>
+      </template>
+    </van-swipe>
+    <template>
+      <div class="swipe_info">
+        <span class="swipe_title">蜘蛛侠:平行宇宙</span>
+        <span class="swipe_score">8.6</span>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Swipe, SwipeItem } from 'vant';
+import { Lazyload } from 'vant';
+
+Vue.use(Lazyload);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+export default {
+  props: ["data"],
+  data() {
+    return {
+      images: [
+        'http://ypyvideo.shigemedia.com/pic/upload/recommend/09606370-62e0-4c35-b322-0fe6d6b7e065.jpg',
+        'http://ypyvideo.shigemedia.com/pic/upload/recommend/ee89e3a1-53d5-4eca-b748-060134366c42.jpg',
+        'http://ypyvideo.shigemedia.com/pic/upload/recommend/5ad7762c-0124-4abf-b04c-edf868edd8a3.jpg',
+      ],
+      mySwipeIndex: 0,
+    };
+  },
+
+  computed: {},
+
+  mounted() { },
+
+  methods: {
+    handleChange(index) {
+      console.log(index);
+      debugger
+      this.mySwipeIndex = index;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/home/screen.scss';
+</style>
