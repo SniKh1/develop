0aae4c571f682f60eb16d65f41d4f7d62ef34010
SniKh1 <1224074384@qq.com>
2020-10-20 13:35:35 +0800

    1

---
+++ src/views/chip_library/chip_library.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div class="chiplibrary-container">
+
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue';
+
+
+export default {
+  props: [],
+  data() { },
+  created() { },
+  computed: {},
+  mounted() { },
+  methods: {},
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/chip_library/chip_library.scss';
+</style>
+<!-- 覆盖子组件样式 -->
+<style lang="scss" >
+</style>
