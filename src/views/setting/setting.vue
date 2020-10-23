4ed10f824e7fa8e41dae78fd5d93662be026255f
SniKh1 <1224074384@qq.com>
2020-10-23 15:37:53 +0800

    1

---
+++ src/views/setting/setting.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div class="setting-container">
+  </div>
+</template>
+
+<script>
+// :oneDirectional="true" 3d空间单向控制 default false
+
+import Vue from 'vue';
+
+
+export default {
+  props: ["data"],
+  data() {
+    return {
+
+    };
+  },
+  created() {
+  },
+  computed: {},
+
+  mounted() {
+
+  },
+
+  methods: {},
+  components: {
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/user/login.scss';
+</style>
+<!-- 覆盖子组件样式 -->
+<style lang="scss" >
+.setting-container {
+}
+</style>
